2ec923b8a43db88229011bf67d4f9e50fec5028d
Leomyn <101626054+Leomyn@users.noreply.github.com>
2022-11-17 12:55:51 +0100

    added preview

--- script.js
+++ script.js
@@ -69,3 +69,29 @@ function createHallo(event){
     main.append(p);
 }
 */
+
+let previewButton = document.getElementById("preview");
+previewButton.addEventListener("click",createPreview);
+function createPreview(){
+    let titel = document.getElementById("title").value;
+    let content = document.getElementById("entryContent").value;
+    let post = document.createElement("div");
+    post.className = "post";
+    let image = document.createElement("img");
+    image.src = "images/clownsmiley.png";
+    let h3 = document.createElement("h3");
+    h3.textContent = titel;
+    let p = document.createElement("p");
+    p.textContent = content;
+    a.append(div);
+    let main = document.createElement("main");
+    document.body.insertBefore(main,footer);
+    main.append(post);
+    post.append(image);
+    post.append(h3);
+    post.append(p);
+    let hr = document.createElement("hr");
+    document.body.insertBefore(hr,main);
+    //Seite lädt neu - Eintrag verschwindet schnell
+    //
+}
